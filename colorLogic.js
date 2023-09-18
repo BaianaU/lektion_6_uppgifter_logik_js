8a4805dce0f0fa648a140e0f2339f7c948c241ad
BaianaU <baianausupbaeva@gmail.com>
2023-09-19 01:53:30 +0200

    First Commit

---
+++ colorLogic.js
@@ -0,0 +1,77 @@
+function changeColor(){
+
+    document.getElementById("my-color-id").style.color = "blue"
+
+}
+
+
+function variables(){
+
+    var helTal = 165
+    var decimalTal = 67.5
+    var text = "Detta är dina mått"
+    var sant = true
+    var falskt = false
+
+    
+
+
+
+}
+
+
+function arrays(){
+
+    var avstånd =([15, 5, 80, 32])
+
+    alert(avstånd[2])
+    alert(avstånd[3])
+    alert(avstånd[0])
+
+}
+
+function userLogin(){
+
+    var svaret = prompt("Vänligen ange inloggningsuppgiter")
+
+    alert (svaret)
+}
+
+
+function initialaVärd(){
+
+    uppgiftArray = [14, 11, 6, 3]
+    sistaElement = uppgiftArray[uppgiftArray.length -1];
+
+    alert(sistaElement)
+
+}
+
+
+    
+
+//document.getElementById("my-color-id").style.color = "blue"
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var helTal = 888;
+
+//var decimalTal = 555.888;
+
+//var text = "Hello world";
+
+//var booleanTrue = true;
+
+//var booleanFalse = false;
+
+
